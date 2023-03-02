aa5f523d78e33d3183e7a16dd24030b479d15228
Abishekh <Abishekh@Abishekh>
2023-03-02 10:52:21 +0530

    initial commit

---
+++ demo/src/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package demo;
+
+public final class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("Hello World");
+	}
+
+}
---
+++ demo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Abishekh
+ *
+ */
+module demo {
+}
